f7c6246012a01f0815b1b00cafa91e8da9b82c27
Gustavo Almagro Rojas <galmagro@gmail.com>
2020-07-25 21:39:30 +0100

    Project skeleton & initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    AppBar,
+    Container,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Theme,
+    Typography
+} from "@material-ui/core";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuIcon from '@material-ui/icons/Menu';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+    ({
+        drawer: {
+            width: 240
+        }
+    })
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [state, setState] = React.useState({
+       visibleDrawer: false
+    });
+
+    const toggleDrawer = (open: boolean) => (
+        event: React.KeyboardEvent | React.MouseEvent,
+    ) => {
+        if (event.type === 'keydown' &&
+            ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, visibleDrawer : open });
+    };
+
+    const classes = useStyles();
+
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        Registro de terrenos
+                    </Typography>
+                </Toolbar>
+                <Container maxWidth="lg">
+
+                </Container>
+                <Drawer open={state.visibleDrawer} onClose={toggleDrawer(false)} classes={{ paper: classes.drawer }}>
+                    <List>
+                        <ListItem>
+                            <ListItemText>Propietarios</ListItemText>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText>Terrenos</ListItemText>
+                        </ListItem>
+                    </List>
+                </Drawer>
+            </AppBar>
+        </div>
+    );
 }
 
 export default App;
